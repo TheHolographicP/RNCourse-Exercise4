4aae395c67e126aab2c0b3d70492015df8425d78
TheHolographicP <thegamblerkid@gmail.com>
2026-03-18 16:23:01 -0600

    Update palette;

--- constants/colors.ts
+++ constants/colors.ts
@@ -1,11 +1,11 @@
 
 
 const Colors = {
-    primary1: '#0D1821',
-    primary2: '#344966',
-    primary3: '#E6AACE',
-    primary4: '#F0F4EF',
-    primary5: '#BFCC94',
+    primary1: '#D7263D',
+    primary2: '#F46036',
+    primary3: '#2E294E',
+    primary4: '#1B998B',
+    primary5: '#C5D86D',
 }
 
 export default Colors
