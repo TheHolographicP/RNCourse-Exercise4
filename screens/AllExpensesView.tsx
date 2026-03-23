3858a14a091d40fa1f73966bbd122fef75559c92
TheHolographicP <thegamblerkid@gmail.com>
2026-03-23 09:43:36 -0600

    AllExpensesView screen implementation complete;

--- screens/AllExpensesView.tsx
+++ screens/AllExpensesView.tsx
@@ -1,14 +1,25 @@
 import { AddExpenseButton } from 'components/AddExpenseButton';
-import { useLayoutEffect } from 'react';
+import { useContext, useLayoutEffect, useState } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+
+import { ExpenseList } from 'components/ExpenseList/ExpenseList';
+import { ExpenseEntry } from 'components/ExpenseEntry/ExpenseEntry';
 
 import type { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
 import type { TabParamList } from 'types/nav';
 
+import { ExpenseContext } from 'store/context/expense-context';
+
 type Props = BottomTabScreenProps<TabParamList, 'AllExpensesView'>;
 
-export function AllExpensesView({ navigation, route }: Props) {
-    
+export function AllExpensesView({ navigation }: Props) {
+    const expenseContext = useContext(ExpenseContext);
+
+    var [expenseEntryOpen, setExpenseEntryOpen] = useState(false);
+
+    var totalExpenses = expenseContext.expenses.reduce((total, expense) => total + expense.value, 0);
+
+
     useLayoutEffect(() => {
         navigation.setOptions({
             headerRight: () => (
@@ -22,12 +33,16 @@ export function AllExpensesView({ navigation, route }: Props) {
     }, [navigation]);
     
     function addExpenseHandler() {
+        setExpenseEntryOpen(true);
     };
-    
+
     return <View style={styles.rootScreen}>
-        <Text>
-            All Expenses View
-        </Text>
+        <ExpenseList
+            expenses={expenseContext.expenses}
+            totalAmount={totalExpenses}
+            headerTitle="All Expenses"
+        />
+        <ExpenseEntry entryActive={expenseEntryOpen} onClose={() => setExpenseEntryOpen(false)} />
     </View>
 }
 
