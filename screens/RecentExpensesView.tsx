746b5d9debbbe3d70a9df4b1529d5d35e1d594c8
TheHolographicP <thegamblerkid@gmail.com>
2026-03-23 09:52:50 -0600

    Implement recent expenses screen;

--- screens/RecentExpensesView.tsx
+++ screens/RecentExpensesView.tsx
@@ -1,13 +1,29 @@
 import { AddExpenseButton } from 'components/AddExpenseButton';
-import { useLayoutEffect } from 'react';
+import { useContext, useLayoutEffect, useState } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
 import type { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
 import type { TabParamList } from 'types/nav';
+import { ExpenseContext } from 'store/context/expense-context';
+
+import { ExpenseList } from 'components/ExpenseList/ExpenseList';
+import { ExpenseEntry } from 'components/ExpenseEntry/ExpenseEntry';
 
 type Props = BottomTabScreenProps<TabParamList, 'RecentExpensesView'>;
 
 export function RecentExpensesView({ navigation }: Props) {
+    const expenseContext = useContext(ExpenseContext);
+    var [expenseEntryOpen, setExpenseEntryOpen] = useState(false);
+
+    const today = new Date();
+    const oneWeekAgo = new Date();
+    oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+
+    const recentExpenses = expenseContext.expenses.filter(
+        expense => expense.date >= oneWeekAgo && expense.date <= today
+    );
+
+    var totalExpenses = recentExpenses.reduce((total, expense) => total + expense.value, 0);
     
     useLayoutEffect(() => {
         navigation.setOptions({
@@ -22,12 +38,16 @@ export function RecentExpensesView({ navigation }: Props) {
     }, [navigation]);
     
     function addExpenseHandler() {
+        setExpenseEntryOpen(true);
     };
 
     return <View style={styles.rootScreen}>
-        <Text>
-            Recent Expenses View
-        </Text>
+        <ExpenseList
+            expenses={recentExpenses}
+            totalAmount={totalExpenses}
+            headerTitle="Last 7 Days"
+        />
+        <ExpenseEntry entryActive={expenseEntryOpen} onClose={() => setExpenseEntryOpen(false)} />
     </View>
 }
 
