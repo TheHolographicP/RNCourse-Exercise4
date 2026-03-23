a14d0bc02923e4d6020f0d3036a18f419f45a845
TheHolographicP <thegamblerkid@gmail.com>
2026-03-23 09:20:59 -0600

    Update total bar styling;

--- components/ExpenseList/TotalBar.tsx
+++ components/ExpenseList/TotalBar.tsx
@@ -10,8 +10,8 @@ type TotalBarProps = {
 
 export function TotalBar({ label, amount }: TotalBarProps) {
     return <View style={styles.totalBar}>
-        <Text>{label}</Text>
-        <Text>{`$${amount.toFixed(2)}`}</Text>
+        <Text style={styles.label}>{label}</Text>
+        <Text style={styles.amount}>{`$${amount.toFixed(2)}`}</Text>
     </View>
 }
 
@@ -19,11 +19,21 @@ const styles = StyleSheet.create({
     totalBar: {
         height: 50,
         width: '100%',
-        backgroundColor: Colors.primary1,
+        backgroundColor: Colors.primary3,
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
         borderRadius: LAYOUT.borderRadius,
         paddingHorizontal: LAYOUT.padding,
+    },
+    label: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: 'white',
+    },
+    amount: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: 'white',
     },
 });
